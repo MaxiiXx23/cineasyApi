6c85978617168501ca38fcd271f2b19533efafb5
userAugustos <felipe_augustos84@outlook.com.br>
2023-01-28 13:57:25 -0300

    feat: adding a entrypoint with some desc

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  entryPoint(): string {
+    return JSON.stringify({
+      visit: 'https://github.com/MaxiiXx23/cineasyApi',
+      version: '0.0.1',
+      name: 'cineasyApi',
+    });
   }
 }
